c9634313e806e2a5d121295f347cccb3e4c11919
Klesio <klesio.k9@gmail.com>
2021-01-02 01:24:19 -0300

    add icons in map

--- public/js/page-create-orphanage.js
+++ public/js/page-create-orphanage.js
@@ -1,12 +1,11 @@
 const options = {
-  dragging: false,
-  touchZoom: false,
-  doubleClickZoom: false,
-  scrollWheelZoom: false,
-  zoomControl: false,
+   dragging: true,
+   touchZoom: true,
+   doubleClickZoom: false,
+   scrollWheelZoom: false,
+   zoomControl: true,
 };
 
-//create map
 const map = L.map("mapid", options).setView([-27.222633, -49.6455874], 15);
 
 //create and titleLayer
@@ -15,48 +14,24 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 //create icon
 
 const icon = L.icon({
-  iconUrl: "./public/img/map-marker.svg",
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [180, 2],
+   iconUrl: "./public/img/map-marker.svg",
+   iconSize: [58, 68],
+   iconAnchor: [29, 68],
 });
 
-//
-const popup = L.popup({
-  closeButton: false,
-  className: "map-popup",
-  minWidth: 240,
-  minHeigth: 240,
-}).setContent(
-  'Lar das meninas <a href= "orphanege.html?id=1" class = "choose-orphanage"> <img src ="./public/img/arrow-white.svg"></a>'
-);
-
 //create and add maker
-L.marker([-27.222633, -49.6455874], { icon }).addTo(map).bindPopup(popup);
 
-function selectImage(event) {
-  const button = event.currentTarget;
+let marker;
 
-  //remover todas as classes .active
-  buttons = document.querySelectorAll(".images button");
-  buttons.forEach(removeActiveClass);
+map.on("click", (event) => {
+   console.log(event);
 
-  function removeActiveClass(button) {
-    button.classList.remove("active");
-    console.log("Active removido com sucesso");
-  }
+   const lat = event.latlng.lat;
+   const lng = event.latlng.lng;
 
-  console.log(button.children);
+   // remove icon
+   marker && map.removeLayer(marker)
 
-  // selecinar imagem clicadas
-
-  const image = button.children[0];
-  const imageContainer  = document.querySelector(".orphanage-datails > img ")
-  //Atualizar o container imagem
-
-  imageContainer.src = image.src
-
-  //adiconar a classe .active para este botão
-
-  button.classList.add("active");
-}
+   //add icon layer
+   marker = L.marker([lat, lng], { icon }).addTo(map);
+});
