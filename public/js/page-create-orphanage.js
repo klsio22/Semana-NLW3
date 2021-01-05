1603cf712046af0732dc0c0cf5602f6fbe8508b2
Klesio <klesio.k9@gmail.com>
2021-01-05 19:56:13 -0300

    fix erro in lat e lng map

--- public/js/page-create-orphanage.js
+++ public/js/page-create-orphanage.js
@@ -20,8 +20,8 @@ map.on("click", (event) => {
 
    const lat = event.latlng.lat;
    const lng = event.latlng.lng;
-   document.querySelector("[name=lat]").value;
-   document.querySelector("[name=lng]").value;
+   document.querySelector("[name=lat]").value = lat;
+   document.querySelector("[name=lng]").value = lng;
 
    // remove icon
    marker && map.removeLayer(marker);
