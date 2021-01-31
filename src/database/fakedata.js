da2dae9ac43a0b7fae8ff5d9e4ae1cf73472f0ca
klesio02 <klesio.k9@gmail.com>
2021-01-31 14:37:34 -0300

    add database fake

---
+++ src/database/fakedata.js
@@ -0,0 +1,43 @@
+module.exports = [
+   {
+      id: 1,
+      lat: "-27.222633",
+      lng: -"49.6455874",
+      name: "Lar das meninas",
+      description:
+         "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+      images: [
+         "https://images.unsplash.com/photo-1609005870928-3fb5409180c0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+
+         " https://images.unsplash.com/photo-1609005870928-3fb5409180c0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+      ],
+
+      instructions:
+         "Venha quando se sentir a vontade e traga muito amor e paciência para dar",
+
+      opening_hours: "Horário de visitas das 18h até 8h",
+
+      open_on_weekends: "1",
+   },
+
+   {
+      id: 2,
+      lat: "-27.222633",
+      lng: -"49.6555874",
+      name: "Lar dos meninos",
+      description:
+         "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+      images: [
+         "https://images.unsplash.com/photo-1609005870928-3fb5409180c0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+
+         " https://images.unsplash.com/photo-1609005870928-3fb5409180c0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+      ],
+
+      instructions:
+         "Venha quando se sentir a vontade e traga muito amor e paciência para dar",
+
+      opening_hours: "Horário de visitas das 18h até 8h",
+
+      open_on_weekends: "0",
+   },
+];
